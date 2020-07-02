84a11f998f8fc9b894fba4a1f8b4fb39ccbf2c56
albertpalacio <albertpalacio@outlook.com>
2020-07-01 21:15:43 -0400

    aboutme componenet added to homepage and styled

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/aboutme.css";
 
 class AboutMe extends Component {
   render() {
-      return(
-          <div>
-              <h1>
-                  About Me Page
-              </h1>
-          </div>
-      )
+    return (
+      <div className="about-bg">
+        <div className="about-div">
+          <h1> About Me </h1>
+          <h4>
+            The name's Albert! I come from a retail background in
+            telecomunications managing small and medium sized teams while
+            exceeding all sales goals set for me. While thinking what to conquer
+            next I decided I would follow something that made me happier early
+            on in life and arrived at Wyncode.
+          </h4>
+          <h4>
+            Here I was able to acquire the knowledge to become a Full Stack Web
+            Developer. I found my love for coding during this time and even more
+            the love for Front-End technologies. s
+          </h4>
+        </div>
+      </div>
+    );
   }
 }
 
-export default AboutMe
+export default AboutMe;
